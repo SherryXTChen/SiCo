09d16f178a470bd604a198b657a2d3a8e6fd03c5
Alex Lim <alex@virtueware.com>
2024-03-19 22:04:47 -0700

    implementing more complex form of instructions

---
+++ app/api/cont/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server';
+import { validate } from 'uuid';
+import prisma from '../../../lib/prisma';
+
+export async function POST(req, res) {
+    try {
+        const data = await req.formData();
+        const uid = data.get('uid');
+        const userImage = data.get('userImage');
+        const firstSite = data.get('firstSite');
+        if(!uid) {
+            return NextResponse.error(new Error('No user id found'));
+        }
+        if(!validate(uid)) {
+            return NextResponse.error(new Error('Invalid user id'));
+        }
+
+        const existsUser = await prisma.user.findUnique({
+            where: {
+                uid: uid,
+            },
+        });
+        if(!existsUser) {
+            await prisma.user.create({
+                data: {
+                    uid: `${uid}`,
+                },
+            });
+        }
+
+        await prisma.user.update({
+            where: {
+                uid: `${uid}`,
+            },
+            data: {
+                userImage: {
+                    create: {
+                        url: `${userImage}`,
+                        firstSite: firstSite === 'true',
+                    },
+                },
+                updatedAt: new Date(),
+            },
+        });
+
+        return NextResponse.json({ message: `Successfully changed image` }, { status: 200 });
+    } catch(err) {
+        console.error('Error processing files:', err);
+        return NextResponse.error(new Error('Error processing files'));
+    }
+}
