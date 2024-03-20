71c092cc9a165b934316819ed0275cf5ebb28baa
Alex Lim <alex@virtueware.com>
2024-03-20 01:25:13 -0700

    updating netcat to use two tcp servers

--- app/api/sendTextToNetcat.js
+++ app/api/sendTextToNetcat.js
@@ -4,7 +4,9 @@ const net = require('net');
 function sendTextToNetcat(textToSend) {
     // Define the address and port of the netcat server
     const host = process.env.BACKBONE_IP;
-    const port = parseInt(process.env.BACKBONE_PORT);
+    const port = textToSend.split("\n")[0] === "scan"
+        ? parseInt(process.env.BACKBONE_SCAN_PORT)
+        : parseInt(process.env.BACKBONE_PORT);
 
     // Create a TCP client
     const client = net.createConnection(port, host, () => {
