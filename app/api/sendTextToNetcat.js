8378b6c4ed4baca31ce67c7dbf3d550fbf92a1a0
Alex Lim <alex@virtueware.com>
2024-03-21 21:43:42 -0700

    added logging info

--- app/api/sendTextToNetcat.js
+++ app/api/sendTextToNetcat.js
@@ -1,10 +1,10 @@
 const net = require('net');
 
 // Function to send text to the netcat server
-function sendTextToNetcat(textToSend) {
+async function sendTextToNetcat(textToSend) {
     // Define the address and port of the netcat server
     const host = process.env.BACKBONE_IP;
-    const port = textToSend.split("\n")[0] === "scan"
+    const port = await textToSend.split("\n")[0] === "scan"
         ? parseInt(process.env.BACKBONE_SCAN_PORT)
         : parseInt(process.env.BACKBONE_PORT);
 
@@ -13,13 +13,17 @@ function sendTextToNetcat(textToSend) {
         console.log('Connected to netcat server');
         // Send the text to the netcat server
         client.write(textToSend);
+        console.log("Sent text to netcat server");
         // Close the connection
         client.end();
+        console.log("Closed connection to netcat server");
+        return "success";
     });
 
     // Handle errors
     client.on('error', (err) => {
         console.error('Error:', err);
+        return "error";
     });
 }
 
