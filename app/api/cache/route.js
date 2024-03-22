4f67de78f8f3c7bf8e2ea0d267ef84ca23fe4f29
Sherry Chen <xchen774@ucsb.edu>
2024-03-22 03:43:19 -0700

    avoid jpeg conversion

--- app/api/cache/route.js
+++ app/api/cache/route.js
@@ -22,8 +22,8 @@ export async function POST(req, res) {
         const userImageBytes = await userImageData.arrayBuffer();
         const userImageBuffer = Buffer.from(userImageBytes);
         const userImagePath = `${uid}/userImage.jpg`;
-        const userImageJpeg = await sharp(userImageBuffer).jpeg().toBuffer();
-        const blob = await put(userImagePath, userImageJpeg, {
+        // const userImageJpeg = await sharp(userImageBuffer).jpeg().toBuffer();
+        const blob = await put(userImagePath, userImageBuffer, {
             access: 'public',
         });
 
