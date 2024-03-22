e81da3eee89ca94ccd873340c4ce49f22010037f
Alex Lim <alex@virtueware.com>
2024-03-21 21:54:22 -0700

    adding logging to check for errors with upload

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -30,6 +30,8 @@ export async function POST(req, res) {
         }
 
         const response = await sendTextToNetcat(`upload\n${uid}\n${userImage}\n${productImage}\n${garmentInfo}\n${firstSite}`);
+        console.log("Response:", response);
+        console.log("Response type:", typeof response);
         if(response !== 'success') {
             return NextResponse.error(new Error('Error processing files'));
         }
