62ecaba5018c7164ead72dbb1353affb0239ec5c
Alex Lim <alex@virtueware.com>
2024-04-04 21:59:22 -0700

    increasing the size of results and minor updates to make recording easier

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -9,14 +9,17 @@ export async function POST(req, res) {
         if(!userImage) {
             return NextResponse.error(new Error('No user image found'));
         }
+        console.log("Got user image");
         const productImage = data.get('productImage');
         if(!productImage) {
             return NextResponse.error(new Error('No product image found'));
         }
+        console.log("Got product image");
         const garmentInfo = data.get('garmentInfo');
         if(!garmentInfo) {
             return NextResponse.error(new Error('No garment info found'));
         }
+        console.log("Got garment info");
         const uid = data.get('uid');
         if(!uid) {
             return NextResponse.error(new Error('No user id found'));
@@ -24,12 +27,16 @@ export async function POST(req, res) {
         if(!validate(uid)) {
             return NextResponse.error(new Error('Invalid user id'));
         }
+        console.log("Got user id");
         const firstSite = data.get('firstSite');
         if(!firstSite) {
             return NextResponse.error(new Error('No first site found'));
         }
+        console.log("Got first site");
 
+        console.log("Sending files to netcat");
         const response = await sendTextToNetcat(`upload\n${uid}\n${userImage}\n${productImage}\n${garmentInfo}\n${firstSite}`);
+        console.log("Got response from netcat:", response);
         if(response !== 'success') {
             return NextResponse.error(new Error('Error processing files'));
         }
